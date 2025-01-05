6ed3b248a7e5a9a1f61f87bc53872634c984c950
Abhishek Malviya <abhishekmalviya997@gmail.com>
2025-01-06 00:11:04 +0530

    project 04

---
+++ react-project-04/propsExtra/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    <div>
+      {/* <h1>This is abhishek</h1> */}
+      {props.children}
+    </div>
+  )
+}
+
+export default Card
