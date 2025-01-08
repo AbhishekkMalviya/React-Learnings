0afd1b2afe4f28a6c300f1ed2a96d796d7057a6b
Abhishek Malviya <abhishekmalviya997@gmail.com>
2025-01-08 22:22:05 +0530

    3rd

---
+++ react-project-03/reactCounterP3/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Counter from './components/Counter'
+
+function App() {
+ 
+  return (
+    <div>
+
+      <h1>Hello Doston.</h1>
+      <Counter/>
+
+    </div>
+  )
+}
+
+export default App
